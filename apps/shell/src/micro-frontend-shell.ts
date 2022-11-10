5c8468720a492da5bb8bfa711d908877e266b3c8
higherkingpud <eddix.ibanez@outlook.jp>
2022-11-10 11:39:16 +0900

    chore(shell): add auth route

--- apps/shell/src/micro-frontend-shell.ts
+++ apps/shell/src/micro-frontend-shell.ts
@@ -9,10 +9,12 @@ const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
   loaders: {
     dashboard: "<h1>Loading Dashboard</h1>",
     menu: "<h1>Loading Menu</h1>",
+    auth: '<h1>Loading Auth</h1>'
   },
   errors: {
     dashboard: "<h1>Failed to load Dashboard</h1>",
     menu: "<h1>Filed to load Menu</h1>",
+    auth: "<h1>Failed to load Auth</h1>"
   },
   props: {},
 });
